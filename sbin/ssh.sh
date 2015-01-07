4652ad3b2e2217e63d5dbfbf6f925996bd4d9e4c
hdcola <huangdong@gmail.com>
2015-01-07 16:59:19 +0800

    确认sshpass不行，更改为直接使用ssh

--- sbin/ssh.sh
+++ sbin/ssh.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+server=$3
+port=$4
+user=$5
+password=$6
+
 CURWDIR=$(cd $(dirname $0) && pwd)
 CUSTOMCONF="$CURWDIR/../conf/custom.conf"
 
+SSHFLAG="-p $password $CURWDIR/../bin/ssh -L *:1080:*:22 -p $port $user@$server -F $CURWDIR/../conf/ssh_config"
+AUTOSSHBIN="$CURWDIR/../bin/autossh"
 
+# set autossh Env var
+export AUTOSSH_GATETIME="30"
+export AUTOSSH_POLL="600"
+export AUTOSSH_PATH="$CURWDIR/../bin/ssh"
+export AUTOSSH_PIDFILE="$CURWDIR/../conf/autossh.pid"
+export OPENSSH_PASSWORD=$password
 
+start(){
+	$AUTOSSHBIN $SSHFLAG
+}
 
 case "$1" in
     "stop")
