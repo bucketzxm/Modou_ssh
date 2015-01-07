f45183870c29794a774c773cf44e12f13a315e15
hdcola <huangdong@gmail.com>
2015-01-07 14:50:29 +0800

    为ssh.sh加入可执行属性

--- sbin/ssh.sh
+++ sbin/ssh.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
-
-
 CURWDIR=$(cd $(dirname $0) && pwd)
 CUSTOMCONF="$CURWDIR/../conf/custom.conf"
 
+
+
+
+case "$1" in
+    "stop")
+        stop;
+        if [[ "0" != "$?" ]]; then
+            exit 1;
+        fi
+        exit 0;
+        ;;
+
+    "start")
+        start;
+        if [[ "0" != "$?" ]]; then
+            exit 1;
+        fi
+        exit 0;
+        ;;
+
+    *)
+        usage init;
+        exit 1;
+        ;;
+esac
