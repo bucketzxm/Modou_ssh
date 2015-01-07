c52cc5ecf510ea763545d556517373355e42d795
hdcola <huangdong@gmail.com>
2015-01-07 17:56:39 +0800

    初步确定ssh.sh

--- sbin/ssh.sh
+++ sbin/ssh.sh
@@ -7,19 +7,26 @@ password=$6
 
 CURWDIR=$(cd $(dirname $0) && pwd)
 CUSTOMCONF="$CURWDIR/../conf/custom.conf"
+PIDFILE="$CURWDIR/../conf/autossh.pid"
 
-SSHFLAG="-p $password $CURWDIR/../bin/ssh -L *:1080:*:22 -p $port $user@$server -F $CURWDIR/../conf/ssh_config"
+#SSHFLAG="-p $password $CURWDIR/../bin/ssh -L *:1080:*:22 -p $port $user@$server -F $CURWDIR/../conf/ssh_config"
+SSHFLAG="-p $password $CURWDIR/../bin/ssh -D 1090 -p $port $user@$server -F $CURWDIR/../conf/ssh_config"
 AUTOSSHBIN="$CURWDIR/../bin/autossh"
 
 # set autossh Env var
 export AUTOSSH_GATETIME="30"
 export AUTOSSH_POLL="600"
 export AUTOSSH_PATH="$CURWDIR/../bin/ssh"
-export AUTOSSH_PIDFILE="$CURWDIR/../conf/autossh.pid"
+export AUTOSSH_PIDFILE=$PIDFILE
 export OPENSSH_PASSWORD=$password
 
 start(){
-	$AUTOSSHBIN $SSHFLAG
+	$AUTOSSHBIN -M 7000 $SSHFLAG
+}
+
+stop(){
+	pid=`cat $PIDFILE 2>/dev/null`;
+    kill $pid >/dev/null 2>&1;
 }
 
 case "$1" in
