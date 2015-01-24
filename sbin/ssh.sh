2e59679cfb12e80f33de5169175ea25a7a8e7036
JunJian <junjian@mochui.net>
2015-01-24 15:48:13 +0800

    修复dnsmasq reload 方式，避免系统dns服务挂掉

--- sbin/ssh.sh
+++ sbin/ssh.sh
@@ -5,9 +5,13 @@ port=$3
 user=$4
 password=$5
 
+# app information
+PACKAGEID="com.modouwifi.vpnssh"
+
 CURWDIR=$(cd $(dirname $0) && pwd)
 CUSTOMCONF="$CURWDIR/../conf/custom.conf"
 CUSTOMBIN="/system/apps/tp/bin/custom"
+CUSTOMPIDFILE="$CURDIR/../conf/custom.pid"
 
 CUSTOMSETCONF="$CURWDIR/../conf/customset.conf"
 SETCONF="$CURWDIR/../conf/set.conf"
@@ -34,9 +38,9 @@ export OPENSSH_PASSWORD=$password
 
 CMDHEAD='"cmd":"'
 CMDTAIL='",'
-SHELLBUTTON1="$CURWDIR/../sbin/ssh.sh starttp"
-SHELLBUTTON2="$CURWDIR/../sbin/ssh.sh stop"
-SHELLBUTTON22="$CURWDIR/../sbin/ssh.sh config"
+SHELLBUTTON1="$CURWDIR/../sbin/ssh.sh config"
+SHELLBUTTON2="$CURWDIR/../sbin/ssh.sh start"
+SHELLBUTTON22="$CURWDIR/../sbin/ssh.sh stop"
 
 CMDBUTTON1=${CMDHEAD}${SHELLBUTTON1}${CMDTAIL}
 CMDBUTTON2=${CMDHEAD}${SHELLBUTTON2}${CMDTAIL}
@@ -52,21 +56,23 @@ usage()
 
 config()
 {
-    generate-config-file $CUSTOMSETCONF 
-    
+    generate-config-file $CUSTOMSETCONF
+
     server=`head -n 1 $CUSTOMSETCONF | cut -d ' ' -f2-`;
     port=`head -n 2 $CUSTOMSETCONF | tail -n 1 | cut -d ' ' -f2-`;
     user=`head -n 3 $CUSTOMSETCONF | tail -n 1 |  cut -d ' ' -f2-`;
     password=`head -n 4 $CUSTOMSETCONF | tail -n 1 | cut -d ' ' -f2-`;
-    
-    #命令中用的是port,user,server,这里直接重新赋值一下
 
+    #命令中用的是port,user,server,这里直接重新赋值一下
 
     /system/sbin/json4sh.sh "set" $DATAJSON service_ip_address value $server
     /system/sbin/json4sh.sh "set" $DATAJSON port_ssh value $port
     /system/sbin/json4sh.sh "set" $DATAJSON user value $user
     /system/sbin/json4sh.sh "set" $DATAJSON password_ssh value $password
 
+    genCustomConfig;
+    pid=`cat $CUSTOMPIDFILE 2>/dev/null`;
+    kill -SIGUSR1 $pid >/dev/null 2>&1;
     return 0;
 }
 
@@ -91,16 +97,16 @@ genRedSocksConfig()
     $REDSOCKSCONF >> $REDSOCKSCONF;
     echo ';' >> $REDSOCKSCONF
     echo '
-            
+
             daemon=on;
             redirector=iptables;
-            }   
+            }
     ' >> $REDSOCKSCONF
-    
+
     echo '
         redsocks {
             local_ip = 192.168.1.1;
-            local_port = 
+            local_port =
     ' >> $REDSOCKSCONF
     $port >> $REDSOCKSCONF
     echo ';' >>$REDSOCKSCONF
@@ -109,9 +115,6 @@ genRedSocksConfig()
         autoproxy=1;
         timeout=5;
     '
-
-
-
 }
 
 startRedSocks()
@@ -135,67 +138,45 @@ startRedSocks()
 
 genCustomConfig()
 {
+    # title   : "SSH VPN"
+    # content : dynamic generated (contains setting info)
+    # button1 : "账号配置"
+    # button2 : "开启/关闭服务" (dynamic switch)
     echo '
     {
-        "title" : "ssh vpn",
+        "title" : "SSH VPN",
     ' > $CUSTOMCONF
 
-	local content=`genCustomContentByName "ssh" "insertinfo"`
-	echo $content >> $CUSTOMCONF
+	  local content=`genCustomContentByName "ssh" "insertinfo" $CUSTOMSETCONF`
+	  echo $content >> $CUSTOMCONF
     echo '
-        
         "button1": {
-
-
     ' >> $CUSTOMCONF
-
     echo $CMDBUTTON1 >> $CUSTOMCONF
-
     echo '
-        "txt" : "启动",
-        "code" : {
-            "0" : "start success",
-            "-1": "start failed"
-        }
-    },
-
-    ' >> $CUSTOMCONF
-
-    echo '
+            "txt": "应用配置",
+            "code": {"0": "正在显示", "-1": "执行失败"}
+            },
         "button2": {
-    ' >> $CUSTOMCONF
-    echo $CMDBUTTON2 >> $CUSTOMCONF
-
+    ' >>$CUSTOMCONF
+    local isserverstart=`checkProcessStatusByName "ssh"`
+    if [ "$isserverstart" == "alive" ]; then
+        echo $CMDBUTTON22 >> $CUSTOMCONF
+        echo '
+            "txt": "关闭服务",
+        ' >> $CUSTOMCONF
+    else
+        echo $CMDBUTTON2 >> $CUSTOMCONF
+        echo '
+            "txt": "开启服务",
+        ' >> $CUSTOMCONF
+    fi
     echo '
-        "txt" : "停止",
-        "code" : {
-            "0" : "stop success",
-            "-1": "stop failed"
-
-        }
-    },
-    ' >> $CUSTOMCONF
-
-    echo '
-        "button3":{
-
-    ' >> $CUSTOMCONF
-    echo $CMDBUTTON22 >> $CUSTOMCONF
-
-    echo '
-        "txt" : "配置",
-        "code": {
-            "0": "",
-            "-1": "配置文件空"
-        }
-
+            "code": {"0": "start success", "-1": "执行失败"}
+            }
     }
-
     ' >> $CUSTOMCONF
-
-    echo '}' >> $CUSTOMCONF
     return 0;
-
 }
 
 checkProcessStatusByName()
@@ -208,41 +189,42 @@ checkProcessStatusByName()
 		echo "alive";
 	fi
 	return 0;
-
 }
 
 
+# operations for generate a "custom" component configfile
+# usage:  genCustomContentByName "ss-redir" "insertinfo" infofile
+#         genCustomContentByName "ss-redir" "noinsertinfo"
 genCustomContentByName()
 {
-    [ "$#" != "2" ] && return 1;
-	local processname="$1"
-	local isinsertinfo="$2"
-	local contenthead='"content":"'
-	local contenttail='",'
-	local contentbody=""
-	local linetag="\n"
-
-	isserverstart=`checkProcessStatusByName $processname`
-
-	if [ "$isserverstart" == "alive" ]; then
-		contentbody="ssh is running"
-	else
-		contentbody="ssh is not running"
-	fi
-
-	if [ "$isinsertinfo" == "insertinfo" ]; then
-		local counts=`cat $CUSTOMSETCONF | wc -l`
-		local configcontent=""
-		for count in $(seq $counts)
-		do
-			line=`head -n $count $CUSTOMSETCONF | tail -n 1`
-			configcontent=${configcontent}${line}${linetag}
-		done
-		contentbody=${contentbody}${linetag}${configcontent}
-		
-	fi
-	echo ${contenthead}${contentbody}${contenttail}
-	return 0;
+    [ "$#" != "3" ] && return 1
+    local processname="$1"
+    local isinsertinfo="$2"
+    local infofile="$3"
+    local contenthead='"content":"'
+    local contenttail='",'
+    local contentbody=""
+    local linetag="\n"
+    isserverstart=`checkProcessStatusByName $processname`
+    if [ "$isserverstart" == "alive" ]; then
+        contentbody="**服务已启动**"
+    else
+        contentbody="**服务未启动**"
+    fi
+    # insert the custom setting info to the content if necessarry
+    # but should pay attention to the '\n'
+    if [ "$isinsertinfo" == "insertinfo" ]; then
+        local counts=`cat $infofile | wc -l`
+        local configcontent=""
+        for count in $(seq $counts)
+        do
+            line=`head -n $count $infofile | tail -n 1`
+            configcontent=${configcontent}${line}${linetag}
+        done
+        contentbody=${contentbody}${linetag}${configcontent};
+    fi
+    echo ${contenthead}${contentbody}${contenttail};
+    return 0;
 }
 
 
@@ -250,10 +232,11 @@ starttp()
 {
     genCustomConfig
     $CUSTOMBIN $CUSTOMCONF
+    echo $! > $CUSTOMPIDFILE
     return 0;
 }
+
 start(){
-    
     if [ ! $server ]; then
         server=`/system/sbin/json4sh.sh get $DATAJSON service_ip_address value`
     fi
@@ -264,19 +247,35 @@ start(){
         user=`/system/sbin/json4sh.sh get $DATAJSON user value`
     fi
     if [ ! $password ]; then
-        password=`/system/sbin/json4sh.sh get $DATAJSON password_ssh value`   
+        password=`/system/sbin/json4sh.sh get $DATAJSON password_ssh value`
     fi
     export OPENSSH_PASSWORD=$password
     SSHFLAG="-N -D *:1080 $user@$server -p $port -F $CURWDIR/../conf/ssh_config"
     $AUTOSSHBIN -M 7000 $SSHFLAG 2>&1 | $ROTATELOGS $ROTATELOGSFLAG $LOG &
-    #genRedSocksConfig
-    #startRedSocks
+    genRedSocksConfig
+    $CURDIR/../sbin/pdns.sh start
+    $CURDIR/../sbin/redsocks.sh start
+    $CURDIR/../sbin/tables.sh start
+    sleep 1
+    genCustomConfig;
+    local pid=`cat $CUSTOMPIDFILE 2>/dev/null`;
+    kill -SIGUSR1 $pid >/dev/null 2>&1;
+    /system/sbin/appInfo.sh set_status $PACKAGEID ISRUNNING
     return 0;
 }
 
 stop(){
     pid=`cat $PIDFILE 2>/dev/null`;
     kill $pid >/dev/null 2>&1;
+    $CURDIR/../sbin/pdns.sh stop
+    $CURDIR/../sbin/redsocks.sh stop
+    $CURDIR/../sbin/tables.sh stop
+
+    sleep 1
+    genCustomConfig;
+    pid=`cat $CUSTOMPIDFILE 2>/dev/null`;
+    kill -SIGUSR1 $pid >/dev/null 2>&1;
+    /system/sbin/appInfo.sh set_status $PACKAGEID NOTRUNNING
     return 0
 }
 
