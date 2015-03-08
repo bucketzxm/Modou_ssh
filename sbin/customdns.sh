5870016121af142aa69df0a9963a15910d12e2a6
JunJian <junjian@mochui.net>
2015-03-08 23:16:36 +0800

    修复

--- sbin/customdns.sh
+++ sbin/customdns.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # app information
-PACKAGEID="com.modouwifi.vpnss"
+PACKAGEID="com.modouwifi.vpnssh"
 
 # get current work path
 local _CURDIR=$(cd $(dirname $0) && pwd)
@@ -14,7 +14,7 @@ local _CUSTOMDNS_TMP="$_CURDIR/../conf/dnsmasq/customdns_tmp.conf"
 
 customdnsInsert(){
   [ -f $_CUSTOMDNS_DATA ] && cp $_CUSTOMDNS_DATA $_CUSTOMDNS
-  local customone=`mci get modou.ssvpn.customdns 2>/dev/null`
+  local customone=`mci get modou.sshvpn.customdns 2>/dev/null`
   if [ "$customone" == "" -o "customdns" = "参考格式:google.com" ]; then
     return 0
   else
@@ -22,20 +22,20 @@ customdnsInsert(){
     dnsmasq -C /var/etc/dnsmasq.conf --test 1>/dev/null 2>&1
     if [ "$?" != "0" ]; then
       rm $_CUSTOMDNS_TMP
-      mci set modou.ssvpn.customdns="格式错误:$customone"
+      mci set modou.sshvpn.customdns="格式错误:$customone"
       mci commit
     else
       local test=`echo $customone | grep ':'`
       if [ "$test" != "" ]; then
         rm $_CUSTOMDNS_TMP
-        mci set modou.ssvpn.customdns="格式错误:$customone"
+        mci set modou.sshvpn.customdns="格式错误:$customone"
         mci commit
         return 0
       fi
       rm $_CUSTOMDNS_TMP
       echo -e "server=/$customone/127.0.0.1#5353" >> $_CUSTOMDNS
       cp $_CUSTOMDNS $_CUSTOMDNS_DATA
-      mci set modou.ssvpn.customdns="成功添加:$customone"
+      mci set modou.sshvpn.customdns="成功添加:$customone"
       mci commit
     fi
   fi
